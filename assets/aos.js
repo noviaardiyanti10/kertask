ee0ec7bdeb0b88e4bb0ef6091ef72b377dae409a
putunovia546 <putunovia546@gmail.com>
2021-12-05 23:47:21 +0800

    change + add

--- assets/aos.js
+++ assets/aos.js
@@ -1,15 +1,4 @@
 AOS.init({
-    duration: 1000,
+    duration: 1200,
 })
-
-// var divs = $('header'),
-//     limit = 300; /* scrolltop value when opacity should be 0 */
-
-// $(window).on('scroll', function() {
-//     var st = $(this).scrollTop();
-
-//     /* avoid unnecessary call to jQuery function */
-//     if (st <= limit) {
-//         divs.css({ 'opacity': (1 - st / limit) });
-//     }
-// });
+window.addEventListener('load', AOS.refresh)
---
+++ assets/index.js
@@ -0,0 +1,12 @@
+let menu_btn = document.querySelector("#menu-btn");
+let user_btn = document.querySelector("#user-btn");
+let sidebar = document.querySelector("#sidebar");
+let footer = document.querySelector("#footer-login")
+let main = document.querySelector(".my-container");
+let nav = document.querySelector(".navbar")
+menu_btn.addEventListener("click", () => {
+    sidebar.classList.toggle("active-bar");
+    nav.classList.toggle("board-nav");
+    main.classList.toggle("main");
+    footer.classList.toggle("footer-user")
+});
